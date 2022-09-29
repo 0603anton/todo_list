bcfcce4e26935b26dcbdfe1f6aaa779f42927a14
0603anton <zorgzb@gmail.com>
2022-09-29 20:30:58 +0200

    2nd lesson Monday - done with mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import './App.css';
-import TodoList, {TaskType} from "./TodoList";
+import TodoList, {TaskType} from './TodoList';
 
 function App() {
     //BLL( Business Logic Layer):
-    const todoListTitle: string = "What to learn";
+    const todoListTitle: string = 'What to learn';
     const tasks: Array<TaskType> = [
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS/TS", isDone: true},
-        {id: 3, title: "React", isDone: false},
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS/TS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
 
     ];
 
-    const todoListTitle_2: string = "what to buy";
+    const todoListTitle_2: string = 'what to buy';
     const tasks_2: Array<TaskType> = [
-        {id: 4, title: "Beer", isDone: true},
-        {id: 5, title: "Cheese", isDone: true},
-        {id: 6, title: "Fish", isDone: false},
+        {id: 4, title: 'Beer', isDone: true},
+        {id: 5, title: 'Cheese', isDone: true},
+        {id: 6, title: 'Fish', isDone: false},
 
     ];
+    //
     //GUI:
     return (
         <div className="App">
